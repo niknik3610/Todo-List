663aa21d5f38e5f325f6ecfd4a1acd1a37b67246
niknik3610 <niklas.harnish@hotmail.com>
2023-04-25 11:46:53 +0200

    date finally works but still kinda borked

---
+++ src/tui_handler/tui_input_handler.rs
@@ -0,0 +1,59 @@
+use super::*;
+
+fn capture_input(
+        sx: &Sender<Event<CEvent::KeyEvent>>,
+        current_tick_time: &mut Instant,
+    ) -> io::Result<()> {
+        let event_timer = MAX_TICK_TIME
+            .checked_sub(current_tick_time.elapsed())
+            .unwrap_or_else(|| Duration::from_secs(0));
+
+        if CEvent::poll(event_timer).expect("Polling Doesn't Work") {
+            if let CEvent::Event::Key(key) = CEvent::read().expect("Reading Events Doesn't Work") {
+                sx.send(Event::Input(key)).expect("Sending Events");
+            }
+        }
+
+        if current_tick_time.elapsed() >= MAX_TICK_TIME {
+            match sx.send(Event::Tick) {
+                Ok(_) => *current_tick_time = Instant::now(),
+                Err(e) => eprintln!("{e}"),
+            }
+        }
+
+        Ok(())
+    }
+
+    fn handle_input(
+        input: CEvent::KeyEvent,
+        current_state: &Arc<Mutex<State>>,
+    ) -> io::Result<UserAction> {
+        use crossterm::event::KeyCode;
+        let current_state_data = current_state.lock().unwrap();
+
+        //handles user actions in normal mode
+        if let State::Viewing = *current_state_data {
+            let key = match input.code {
+                KeyCode::Char(input) => input,
+                _ => return Ok(UserAction::None),
+            };
+
+            return match key {
+                'q' => Ok(UserAction::Quit),
+                'n' => Ok(UserAction::AddTodo),
+                'c' => Ok(UserAction::CompeleteTodo),
+                'u' => Ok(UserAction::UncompleteTodo),
+                _ => Ok(UserAction::None),
+            };
+        }
+
+        //handles user actions when in buffer mode
+        use BufferAction::*;
+        match input.code {
+            KeyCode::Char(input) => return Ok(UserAction::ManipulateBuffer(Input(input))),
+            KeyCode::Backspace => return Ok(UserAction::ManipulateBuffer(Backspace)),
+            KeyCode::Enter => return Ok(UserAction::ManipulateBuffer(SubmitBuffer)),
+            KeyCode::Esc => return Ok(UserAction::ManipulateBuffer(ExitBuffer)),
+            _ => return Ok(UserAction::None),
+        };
+    }
