5dfd2d14437a8f15a9430f37b9672a53424a6ffb
niknik3610 <niklas.harnish@hotmail.com>
2023-05-03 11:37:41 +0200

    fixed broked main :|

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,14 @@ mod tui_handler;
 mod parsing_handler;
 
 fn main() {
-<<<<<<< HEAD
-    env::set_var("RUST_BACKTRACE", "1");
-    
-=======
     // let parsed = parsing_handler::parse("").unwrap();
     // println!("{:?}", parsed);
     let mut file = home_dir().expect("Could not find home directory");       
     file.push(".todo_items");
->>>>>>> 6a76d04 (now saves the todolist file into the home directory under .todo_items)
     //error handling the load file
     let mut list = load_todo_list(&file).unwrap();
     tui_handler::tui_handler::run_tui(&mut list).unwrap();
-<<<<<<< HEAD
-    
-    
-    save_todo_list(&list).unwrap();
-=======
 
     //For testing purposes not saving currently, uncomment to enable saving list
     save_todo_list(&list, file).unwrap();
->>>>>>> 6a76d04 (now saves the todolist file into the home directory under .todo_items)
 }
